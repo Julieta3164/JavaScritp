8468058e7647d50dbbc166d4f727beb7946b6369
Juliet Linarez <julietlinarez@Mac-mini-de-Heber.local>
2022-08-08 10:45:28 +0200

    styles

--- js-parte2/app.js
+++ js-parte2/app.js
@@ -10,18 +10,3 @@ function comparar(){
     }
     document.getElementById("comparacion").innerHTML= "Este es el mayor: " +mayor;
 }
-//Escribe un programa que pida 3 números y escriba en la pantalla el mayor de los tres.
-/* function comparar(){
-    let numero1=parseFloat(document.getElementById("numb1").value);
-    let numero2=parseFloat(document.getElementById("numb2").value);
-    let numero3=parseFloat(document.getElementById("numb3").value);
-    let mayor=0;
-
-    if (numero1>numero2){
-        mayor=numero1;
-    }else (numero1>numero3){
-        mayor=numero3;
-    }
-
-document.getElementById("comparacion").innerHTML= "Este es el mayor: ";
-} */
