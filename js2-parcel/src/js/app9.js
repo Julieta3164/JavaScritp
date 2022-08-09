c16889d334c05aa464ffb455f48b0b881512542e
Juliet Linarez <julietlinarez@Mac-mini-de-Heber.local>
2022-08-09 10:27:53 +0200

    exercise  9 check

--- js2-parcel/src/js/app9.js
+++ js2-parcel/src/js/app9.js
@@ -1,31 +1,31 @@
-//Añadir al ejercicio anterior que nos diga por cuál de los cuatro es divisible (hay que decir todos por los que es divisible)
-const usedInput = document.getElementById("input9");
-const button = document.getElementById("btn9");
-const resultado = document.getElementById("text9");
 
-
-
-function whichOneIsBigger(numbOne){
-    if (numbOne % 2 === 0 || numbOne % 3 === 0 || numbOne % 5 === 0 || numbOne % 7 === 0) {
-        if (numbOne % 2 === 0) {
-            resultado.innerHTML = "El número si es divisible por 2 ";
-        }
-        if (numbOne % 3 === 0) {
-            resultado.innerHTML = "El número si es divisible por 3 ";
-        }
-        if (numbOne % 5 === 0) {
-            resultado.innerHTML = "El número si es divisible por 5 ";
-        }
-        if (numbOne % 7 === 0) {
-            resultado.innerHTML = "El número si es divisible por 7 ";
-        }
-        } else {
-            resultado.innerHTML = "El número no es divisible ni por 2 ni por 3 ni por 5 ni por 7 ";
-        }
-
-button.addEventListener("click", () =>{
-    let numbOneUser = usedInput.value;
-    whichOneIsBigger(numbOneUser);
-    
+let input = document.getElementById('input9');
+let btn9 = document.getElementById('btn9');
+let text9 = document.getElementById('text9');
+        
+btn9.addEventListener('click', () => {
+let input9 = input.value;
+let result = divisibility(input9);
+text9.innerHTML = `Resultado: ${result}`; 
 });
-}
+        
+        function divisibility(input9) {
+        
+            if (input9 % 2 === 0 || input9 % 3 === 0 || input9 % 5 === 0 || input9 % 7 === 0) {
+                if (input9 % 2 === 0) {
+                    document.write(`Resultado: ${input9} Es divisible entre 2 <br/>`);
+                }
+                if (input9 % 3 === 0) {
+                    document.write(`Resultado: ${input9} Es divisible entre 3 <br/>`);
+                }
+                if (input9 % 5 === 0) {
+                    document.write(`Resultado: ${input9} Es divisible entre 5 <br/>`);
+                }
+                if (input9 % 7 === 0) {
+
+                    document.write(`Resultado: ${input9} Es divisible entre 7 <br/>`);
+                }
+            } else {
+                return("No es divisible ni entre 2, ni entre 3, ni entre 5, ni entre 7");
+            }
+        }; 
