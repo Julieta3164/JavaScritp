4093f6d512dd8201679d6e7f0f5e4da150c9a16d
Juliet Linarez <julietlinarez@Mac-mini-de-Heber.local>
2022-08-09 10:07:44 +0200

    exercise  8 check

--- js2-parcel/src/js/app8.js
+++ js2-parcel/src/js/app8.js
@@ -1,22 +1,23 @@
 //Escribe un programa que pida un número y nos diga si es divisible por 2, 3, 5 o 7 (sólo hay que comprobar si lo es por uno de los cuatro)
-const usedInput = document.getElementById("input8");
-const button = document.getElementById("btn8");
-const resultado = document.getElementById("text8");
 
-
-
-function whichOneIsBigger(numbOne){
-    if (numbOne % 2 === 0 || numbOne % 3 === 0 || numbOne % 5 === 0 || numbOne % 7 === 0) {
-    
-    resultado.innerHTML = "El número si es divisible"
-    
-} else{
-    resultado.innerHTML = "El número no es divisible"
+let btn8 = document.getElementById('btn8');
+btn8.addEventListener('click',findDivisor);
+console.log(btn8)
+function findDivisor(){ 
+    let input8 = document.getElementById('input8').value;
+    let text8 = document.getElementById('text8');
+    let divisor = []
+    if( input8 % 2 == 0){
+        text8.innerHTML=`${input8}es divisible por 2`
+        divisor.push(input8)
+    };
+    if(input8 % 3  === 0 ){
+        text8.innerHTML=`${input8}es divisible por 3`
+    };
+    if(input8 % 5  === 0 ){
+        text8.innerHTML=`${input8}es divisible por 5`
+    };
+    if(input8 % 7  === 0 ){
+        text8.innerHTML=`${input8}es divisible por 7`
+    };
 }
-    
-}
-btn8.addEventListener("click", () =>{
-    let numbOneUser = usedInput.value;
-    whichOneIsBigger(numbOneUser);
-    
-});
