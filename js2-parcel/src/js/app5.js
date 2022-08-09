cfc85fd570c460b14a4428b47d4e28425bbb0688
Juliet Linarez <julietlinarez@Mac-mini-de-Heber.local>
2022-08-09 09:32:04 +0200

    corrections in exercise 5

--- js2-parcel/src/js/app5.js
+++ js2-parcel/src/js/app5.js
@@ -1,43 +1,21 @@
 //Escribe un programa que pida una frase y escriba las vocales que aparecen
 
-let userInput = document.getElementById("vowels");
-let button = document.getElementById("btn-vowels");
-let resultado = document.getElementById("text-vowels")
+let btn = document.getElementById('btn5');
+btn5.addEventListener('click',Search);
 
-function consultText() {
+function Search () {
+    let input5 = document.getElementById("input5").value;
+    let count = [];
 
-    let separateText = userInput.value.toLowerCase();
+	for(let i = 0; i < input5.length; i++) {
+		if (input5[i] === "a") count.push("a");
+		if (input5[i] === "e") count.push("e");
+		if (input5[i] === "i") count.push("i");
+		if (input5[i] === "o") count.push("o");
+		if (input5[i] === "u") count.push("u");
+	}
+	let totalVocals = document.getElementById("text5").innerHTML = count;
+    text5.innerHTML = `las vocales que aparecen son : ${totalVocals}`
+} 
 
-    return separateText;
-}
-function searchForVowels() {
 
-    let a, e, i, o, u
-    a = e = i = o = u = []
-    let Letters = consultText();
-
-
-    for (k = 0; k < Letters.length; k++) {
-
-        switch (Letters[k]) {
-            case "a":
-                a.push("a");
-                break;
-            case "e":
-                e.push("e");
-                break;
-            case "i":
-                i.push("i");
-                break;
-            case "o":
-                o.push("o");
-                break;
-            case "u":
-                u.push("u");
-                break;
-        }
-    }
-    resultado.innerHTML = `Tenemos: ${a} `
-}
-
-button.addEventListener("click", searchForVowels)
